26ca1050186a38ebdeaf0f01df528290c1f473ca
Diana <dianaeparra@misena.edu.co>
2015-06-05 12:46:48 -0500

    Actualizacion

--- dianaphp/CAPITULO4/formularioaltausuario.php
+++ dianaphp/CAPITULO4/formularioaltausuario.php
@@ -2,6 +2,7 @@
 <BODY>
 	<form action="crearusuario.php" method="post">
 	<input type="text" name="usuario" value="introduce tu usuario" size="40"><br>
+	<input type="text" name="contraseña" value="introduce tu contraseña" size="40"><br>
 	
 </body>
 </html>
